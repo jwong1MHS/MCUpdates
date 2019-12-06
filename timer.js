8c5d7cc8f072be81f165680e24eb42bcf1283879
jwong1MHS <jasonw5172001@gmail.com>
2019-12-06 02:53:31 -0500

    Added new revisions

--- timer.js
+++ timer.js
@@ -8,19 +8,18 @@ var x = setInterval(function() {
     var now = new Date().getTime();
 	
     // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    var remaining = countDownDate - now;
     
     // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var days = Math.floor(remaining / (1000 * 60 * 60 * 24)); //1000 ms, 60s, 60m, 24hr
+    var hours = Math.floor((remaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); //Remove days, divided 1000ms, 60s, 60m
+    var minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60)); //Remove hours, divided 1000ms, 60s
+    var seconds = Math.floor((remaining % (1000 * 60)) / 1000); //Remove minutes, divided 1000ms
     
-    // Output the result in an element with id="myTimer"
-    document.getElementById("myTimer").innerHTML = days + "d " + hours + "h "
-	    + minutes + "m " + seconds + "s ";
+    // Output the result
+    document.getElementById("myTimer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    // If the count down is over, write some text 
+    // If the count down is over, print text
 	if (distance < 0) {
 		clearInterval(x);
 		document.getElementById("myTimer").innerHTML = "Movie Up!";
